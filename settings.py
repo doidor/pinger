2572a0ed08df2dd56efc78e7c709a69413d279f7
Tudor Popa <tdr.popa@gmail.com>
2013-10-19 19:41:32 +0300

    added the settings.py file

---
+++ settings.py
@@ -0,0 +1,15 @@
+
+MAIL_SETTINGS = {
+	"from" : "DoiDor Pinger <pinger@ssd.doidor.com>",
+	"to" : "tdr.popa@gmail.com",
+	"subject" : "Failed sites"
+}
+
+PING_TIMEOUT = 10
+
+URLS_TO_PING  = [
+        'http://proalba.ro/',
+        'http://alba-sport.ro/',
+        'http://hipponet.ro/'
+]
+
